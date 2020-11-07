4d616d4b1d36b5eaabd3443c6d151bd97262032e
Damir Minnegalimov <damikdk@gmail.com>
2020-11-07 05:49:49 +0300

    Initial Commit

---
+++ Nia Editor/Nia_EditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nia_EditorApp.swift
+//  Nia Editor
+//
+//  Created by Damik Minnegalimov on 07.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct Nia_EditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
