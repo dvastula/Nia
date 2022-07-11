7db9ac698f89f2e6d73fb50df7cb5ab2819b2965
Damik Minnegalimov <damikdk@gmail.com>
2022-07-12 01:45:42 +0300

    Make a whole preview movable

--- Nia Editor/Screens/PreviewScreen.swift
+++ Nia Editor/Screens/PreviewScreen.swift
@@ -14,159 +14,169 @@ struct PreviewScreen: View {
   @EnvironmentObject var currentEditor: Editor
   @State private var selectedPhotos: [PhotosPickerItem] = []
   @State private var lastTimeImported: Date = Date()
- 
+  
   @State var lastSnapshotURL: URL?
   @State private var showingExportSheet = false
-    
-    @State private var tempScale: CGFloat = 1.0
-    @State private var tempOffset: CGSize = .zero
-    @State private var tempRotation: Angle = .degrees(0)
-
-    @State private var scale: CGFloat = 1.0
-    @State private var offset: CGSize = .zero
-    @State private var rotation: Angle = .degrees(0)
-
+  
+  @State private var tempScale: CGFloat = 1.0
+  @State private var tempOffset: CGSize = .zero
+  @State private var tempRotation: Angle = .degrees(0)
+  
+  @State private var scale: CGFloat = 1.0
+  @State private var offset: CGSize = .zero
+  @State private var rotation: Angle = .degrees(0)
+  
   var mainView: some View {
     PreviewView(currentEditor: _currentEditor)
   }
-
+  
   var body: some View {
-      let rotationGesture = RotationGesture(minimumAngleDelta: .degrees(1))
-        .onChanged { angle in
-          rotation = angle + tempRotation
-        }
-        .onEnded { angle in
-            tempRotation = rotation
-        }
-      
-      let dragGesture = DragGesture()
-        .onChanged { gesture in
-          offset = gesture.translation + tempOffset
-        }
-        .onEnded { gesture in
-            tempOffset = offset
-        }
-      
-      let scaleGesture = MagnificationGesture()
-        .onChanged { magnification in
-          scale = magnification * tempScale
-        }
-        .onEnded { angle in
-            tempScale = scale
-        }
-      
-      let allGestures = dragGesture
-        .simultaneously(with: rotationGesture)
-        .simultaneously(with: scaleGesture)
-
-      ZStack {
-          GeometryReader { geometry in
-              
-            // Preview component with content itself
-            mainView
-              .onAppear() {
-                  tempScale = currentEditor.size.aspectFitRatio(inside: geometry.size)
-                  tempOffset = geometry.size / 2
-                  
-                  scale = tempScale
-                  offset = tempOffset
-              }
-//                  .onChange(of: geometry.size) { newSize in
-//                    scale = currentEditor.size.aspectFitRatio(inside: newSize)
-//                  }
-
-              .frame(width: currentEditor.size.width,
-                     height: currentEditor.size.height)
-              .scaleEffect(scale)
-              .rotationEffect(rotation)
-              .offset(offset)
-              .position(
-                x: 0,
-                y: 0)
-              
-              .gesture(allGestures)
-              
-              .animation(Animation.easeInOut(duration: 0.15), value: offset)
-              .animation(Animation.easeInOut(duration: 0.15), value: scale)
-              .animation(Animation.easeInOut(duration: 0.15), value: rotation)
-          }
+    let rotationGesture = RotationGesture(minimumAngleDelta: .degrees(1))
+      .onChanged { angle in
+        rotation = angle + tempRotation
+      }
+      .onEnded { angle in
+        tempRotation = rotation
+      }
+    
+    let dragGesture = DragGesture()
+      .onChanged { gesture in
+        offset = gesture.translation + tempOffset
+      }
+      .onEnded { gesture in
+        tempOffset = offset
+      }
+    
+    let scaleGesture = MagnificationGesture()
+      .onChanged { magnification in
+        scale = magnification * tempScale
+      }
+      .onEnded { angle in
+        tempScale = scale
+      }
+    
+    let allGestures = dragGesture
+      .simultaneously(with: rotationGesture)
+      .simultaneously(with: scaleGesture)
+    
+    ZStack {
+      GeometryReader { geometry in
         
-        
-        // Action panel
-        
-        FloatPanel(.bottomLeading) {
-                              
-          if !currentEditor.layers.isEmpty {
-            
-            // Share button
-            
-            Button {
-              Task {
-                lastSnapshotURL = Exporter.export(mainView.environmentObject(currentEditor))
-                showingExportSheet.toggle()
-              }
-            } label: {
-              Image(systemName: "square.and.arrow.up")
-                .font(.largeTitle)
+        ZStack {
+          // Preview component with content itself
+          mainView
+            .onAppear() {
+              tempScale = currentEditor.size.aspectFitRatio(inside: geometry.size)
+              tempOffset = geometry.size / 2
+              
+              scale = tempScale
+              offset = tempOffset
             }
-            .buttonStyle(FloatButton())
-
-            
-            // Remove all button
-
-            Button {
-              withAnimation { () -> () in
-                currentEditor.removeAll()
-              }
-            } label: {
-              Image(systemName: "trash.fill")
-                .font(.largeTitle)
-            }
-            .buttonStyle(FloatButton())
-          }
+//            .onChange(of: geometry.size) { newSize in
+//              scale = currentEditor.size.aspectFitRatio(inside: newSize)
+//            }
           
-          // Add asset button
+            .frame(width: currentEditor.size.width,
+                   height: currentEditor.size.height)
+            .scaleEffect(scale)
+            .rotationEffect(rotation)
+            .offset(offset)
           
-          Button {} label: {
-            PhotosPicker(selection: $selectedPhotos) {
-              Image(systemName: "plus")
-                .font(.largeTitle)
+            .position(
+              x: 0,
+              y: 0)
+          
+        }
+        .frame(
+          maxWidth: .infinity,
+          maxHeight: .infinity,
+          alignment: .topLeading
+        )
+        .background(Color.gray.opacity(0.3))
+      }
+      
+      // Action panel
+      
+      FloatPanel(.bottomLeading) {
+        
+        if !currentEditor.layers.isEmpty {
+          
+          // Share button
+          
+          Button {
+            Task {
+              lastSnapshotURL = Exporter.export(mainView.environmentObject(currentEditor))
+              showingExportSheet.toggle()
             }
+          } label: {
+            Image(systemName: "square.and.arrow.up")
+              .font(.largeTitle)
+          }
+          .buttonStyle(FloatButton())
+          
+          
+          // Remove all button
+          
+          Button {
+            withAnimation { () -> () in
+              currentEditor.removeAll()
+            }
+          } label: {
+            Image(systemName: "trash.fill")
+              .font(.largeTitle)
           }
           .buttonStyle(FloatButton())
         }
-        .padding()
-
-      }
-      .onChange(of: selectedPhotos) { newItems in
-        // Stupid hack because onChange is called million times with same values
-        if Int(Date().timeIntervalSince(lastTimeImported)) < 1 {
-          print("onChange was fired too fast, skip")
-          return
-        }
         
-        lastTimeImported = Date()
+        // Add asset button
         
-        Task {
-          try await onImageSelect(newItems)
+        Button {} label: {
+          PhotosPicker(selection: $selectedPhotos) {
+            Image(systemName: "plus")
+              .font(.largeTitle)
+          }
         }
+        .buttonStyle(FloatButton())
       }
-      .sheet(isPresented: $showingExportSheet) {
+      .padding()
+      
+    }
+    
+    .gesture(allGestures)
+    
+    .animation(Animation.easeInOut(duration: 0.15), value: offset)
+    .animation(Animation.easeInOut(duration: 0.15), value: scale)
+    .animation(Animation.easeInOut(duration: 0.15), value: rotation)
+    
+    .onChange(of: selectedPhotos) { newItems in
+      // Stupid hack because onChange is called million times with same values
+      if Int(Date().timeIntervalSince(lastTimeImported)) < 1 {
+        print("onChange was fired too fast, skip")
+        return
+      }
+      
+      lastTimeImported = Date()
+      
+      Task {
+        try await onImageSelect(newItems)
+      }
+    }
+    .sheet(isPresented: $showingExportSheet) {
+      
+      if let lastSnapshotURL {
+        let uiImage = UIImage(contentsOfFile: lastSnapshotURL.path())!
+        let image = Image(uiImage:uiImage)
         
-        if let lastSnapshotURL {
-          let uiImage = UIImage(contentsOfFile: lastSnapshotURL.path())!
-          let image = Image(uiImage:uiImage)
-          
-          ExportView(
-            showing: $showingExportSheet,
-            image: image,
-            fileURL: lastSnapshotURL)
-        }
+        ExportView(
+          showing: $showingExportSheet,
+          image: image,
+          fileURL: lastSnapshotURL)
       }
+    }
   }
   
   private func onImageSelect(_ newItems: [PhotosPickerItem]) async throws {
-      
+    
     for item in newItems {
       
       let newAsset = try await Importer.import(item)
